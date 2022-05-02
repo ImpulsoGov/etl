386065202af069f2d676ee29580f5bd057eb955b
Walter <wmatheus.97@gmail.com>
2022-05-02 17:01:02 -0300

    Altera formato da competncia para objeto date

--- src/impulsoetl/sisab/parametros_cadastro/principal.py
+++ src/impulsoetl/sisab/parametros_cadastro/principal.py
@@ -1,41 +1,24 @@
-# SPDX-FileCopyrightText: 2021, 2022 ImpulsoGov <contato@impulsogov.org>
-#
-# SPDX-License-Identifier: MIT
-
-
 from __future__ import annotations
-
 from sqlalchemy.orm import Session
-<<<<<<< HEAD
 from datetime import date
-=======
-
->>>>>>> f19009cf7eaf7e0e5cf4c3b1035b8d06a05c0a6d
 from impulsoetl.sisab.parametros_cadastro.carregamento import (
-    carregar_parametros,
+    carregar_parametros ,
 )
-from impulsoetl.sisab.parametros_cadastro.extracao import extrair_parametros
+from impulsoetl.sisab.parametros_cadastro.extracao import (
+    extrair_parametros,
+)
 from impulsoetl.sisab.parametros_cadastro.teste_validacao import (
     teste_validacao,
 )
-<<<<<<< HEAD
 from sisab.parametros_cadastro.tratamento import tratamento_dados
 from impulsoetl.bd import Sessao
-=======
-from impulsoetl.sisab.parametros_cadastro.tratamento import tratamento_dados
-from impulsoetl.tipos import DatetimeLike
->>>>>>> f19009cf7eaf7e0e5cf4c3b1035b8d06a05c0a6d
 
 def obter_parametros(
     sessao: Session,
     visao_equipe: str,
     periodo: date,
     nivel_agregacao: str,
-<<<<<<< HEAD
     teste: bool = True
-=======
-    teste: bool = False,
->>>>>>> f19009cf7eaf7e0e5cf4c3b1035b8d06a05c0a6d
 ) -> None:
     """Extrai, transforma e carrega dados de parâmetros cadastros de equipes pelo SISAB.
     Argumentos:
@@ -56,28 +39,11 @@ def obter_parametros(
             posterior ao método [`Session.rollback()`][] da sessão gerada com o
             SQLAlchemy."""
 
-    df = extrair_parametros(
-        visao_equipe=visao_equipe,
-        competencia=periodo,
-        nivel_agregacao=nivel_agregacao,
-    )
-    df_tratado = tratamento_dados(
-        sessao=sessao,
-        dados_sisab_cadastros=df,
-        periodo=periodo,
-        nivel_agregacao=nivel_agregacao,
-    )
-    teste_validacao(df, df_tratado, nivel_agregacao=nivel_agregacao)
-    carregar_parametros(
-        sessao=sessao,
-        parametros_transformada=df_tratado,
-        visao_equipe=visao_equipe,
-        nivel_agregacao=nivel_agregacao,
-    )
+    df = extrair_parametros(visao_equipe=visao_equipe,competencia=periodo,nivel_agregacao=nivel_agregacao)
+    df_tratado = tratamento_dados(sessao=sessao,dados_sisab_cadastros=df,periodo=periodo,nivel_agregacao=nivel_agregacao)
+    teste_validacao(df, df_tratado,nivel_agregacao=nivel_agregacao)
+    carregar_parametros(sessao=sessao,parametros_transformada=df_tratado,visao_equipe=visao_equipe,nivel_agregacao=nivel_agregacao)
     if not teste:
         sessao.commit()
-<<<<<<< HEAD
 
 
-=======
->>>>>>> f19009cf7eaf7e0e5cf4c3b1035b8d06a05c0a6d
