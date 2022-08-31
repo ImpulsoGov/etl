38a01e4b37ba5e338a59ee264bf3e969664d74d9
Walter Matheus <86883404+WaltMath@users.noreply.github.com>
2022-08-31 18:37:56 -0300

    refactor(sisab): :recycle: Restrutura ETLs de relatórios de validação do SISAB (#30)
    * Restruturação ETL para relatório de validação
    
    * Correções do ETL de Validação da Produção
    
    * Corrigi problemas de estilo e convenções
    
    * Corrige parâmetro 'envioPrazo' na requisição
    
    * style(sisab): :art: Aplica formatadores isort e black.
    
    Co-authored-by: bcbernardo <bernardo@impulsogov.org>

---
+++ src/impulsoetl/sisab/relatorio_validacao_producao/tratamento.py
@@ -0,0 +1,108 @@
+# SPDX-FileCopyrightText: 2021, 2022 ImpulsoGov <contato@impulsogov.org>
+#
+# SPDX-License-Identifier: MIT
+
+
+"""Processa dados do relatório de validação por produção para o formato usado no BD."""
+
+from __future__ import annotations
+
+from typing import Final
+
+import numpy as np
+import pandas as pd
+from frozendict import frozendict
+from sqlalchemy.orm import Session
+
+from impulsoetl.comum.geografias import id_sus_para_id_impulso
+from impulsoetl.loggers import logger
+
+VALIDACAO_TIPOS: Final[frozendict] = frozendict(
+    {
+        "municipio_id_sus": str,
+        "periodo_id": str,
+        "cnes_id": str,
+        "cnes_tipo": str,
+        "ine_id": str,
+        "ine_tipo": str,
+        "ficha": str,
+        "aplicacao": str,
+        "validacao_nome": str,
+        "validacao_quantidade": int,
+        "periodo_codigo": str,
+        "no_prazo": bool,
+        "unidade_geografica_id": str,
+    }
+)
+
+VALIDACAO_COLUNAS: Final[dict[str, str]] = {
+    "IBGE": "municipio_id_sus",
+    "CNES": "cnes_id",
+    "INE": "ine_id",
+    "Validação": "validacao_nome",
+    "Tipo Equipe": "ine_tipo",
+    "Tipo Unidade": "cnes_tipo",
+    "Total": "validacao_quantidade",
+}
+
+
+def tratamento_dados(
+    sessao: Session,
+    df_extraido: str,
+    periodo_id: str,
+    periodo_codigo: str,
+    envio_prazo: bool,
+    ficha: str,
+    aplicacao: str,
+) -> pd.DataFrame:
+    """Trata dados capturados do relatório de indicadores do SISAB
+
+    Argumentos:
+        sessao: objeto [`sqlalchemy.orm.session.Session`][] que permite
+            acessar a base de dados da ImpulsoGov.
+        df_extraido: [`DataFrame`][] contendo os dados capturados no relatório de Indicadores do Sisab
+         (conforme retornado pela função
+            [`extrair_dados()`][]).
+        periodo_competencia: Data do quadrimestre da competência em referência
+        envio_prazo: Indica se os relatórios de validação a serem considerados
+        apenas os enviados no prazo (`True`) ou se devem considerar tanto
+        envios no prazo quanto fora do prazo (`False`).
+        ficha: Nome da ficha requisitada
+        aplicacao: Nome da aplicacao requisitada
+
+    Retorna:
+        Objeto [`pandas.DataFrame`] com os dados enriquecidos e tratados.
+
+            [`sqlalchemy.orm.session.Session`]: https://docs.sqlalchemy.org/en/14/orm/session_api.html#sqlalchemy.orm.Session
+            [`pandas.DataFrame`]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
+    """
+
+    logger.info("Iniciando tratamento dos dados...")
+    logger.info("Renomeando colunas...")
+    df_tratado = df_extraido.rename(columns=VALIDACAO_COLUNAS)
+
+    logger.info("Enriquecendo tabela com campos complementares...")
+    df_tratado["no_prazo"] = envio_prazo
+    df_tratado["ficha"] = ficha
+    df_tratado["aplicacao"] = aplicacao
+    df_tratado["periodo_codigo"] = periodo_codigo
+    df_tratado["periodo_id"] = periodo_id
+    df_tratado["unidade_geografica_id"] = df_tratado["municipio_id_sus"].apply(
+        lambda municipio_id_sus: id_sus_para_id_impulso(
+            sessao=sessao,
+            id_sus=municipio_id_sus,
+        )
+    )
+
+    df_tratado.reset_index(drop=True, inplace=True)
+
+    logger.info("Garantindo tipagem dos dados...")
+    df_tratado = df_tratado.astype(VALIDACAO_TIPOS)
+
+    df_tratado[["cnes_tipo", "ine_tipo", "ine_id"]] = df_tratado[
+        ["cnes_tipo", "ine_tipo", "ine_id"]
+    ].replace("nan", np.nan)
+    logger.info(
+        f"Tratamento dos dados realizado | Total de registros : {df_tratado.shape[0]}"
+    )
+    return df_tratado
