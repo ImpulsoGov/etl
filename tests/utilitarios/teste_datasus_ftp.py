7b4d9367001503a4045387e0152110029c7def9a
bcbernardo <bernardo@impulsogov.org>
2022-08-31 21:44:36 -0300

    feat(impulsoetl): :sparkles: Adiciona suporte a regex na extração de arquivos dos FTPs do DataSUS

--- tests/utilitarios/teste_datasus_ftp.py
+++ tests/utilitarios/teste_datasus_ftp.py
@@ -6,10 +6,59 @@
 """Casos de teste para funções utilitárias relacionadas ao FTP do DataSUS."""
 
 
+import re
+from ftplib import FTP, error_perm
+
 import pandas as pd
 import pytest
 
-from impulsoetl.utilitarios.datasus_ftp import extrair_dbc_lotes
+from impulsoetl.utilitarios.datasus_ftp import (
+    _listar_arquivos,
+    extrair_dbc_lotes,
+)
+
+
+@pytest.fixture(scope="function")
+def cliente_ftp_siasus():
+    try:
+        ftp = FTP("ftp.datasus.gov.br")
+        ftp.login()
+        ftp.cwd("/dissemin/publicos/SIASUS/200801_/Dados")
+        yield ftp
+    finally:
+        ftp.quit()
+
+
+@pytest.mark.parametrize(
+    "arquivo_nome_ou_padrao",
+    ["PARR2108.dbc", re.compile("PASP1112[a-z]?.dbc", re.IGNORECASE)]
+)
+def teste_listar_arquivos_existentes(
+    cliente_ftp_siasus,
+    arquivo_nome_ou_padrao,
+):
+    lista_arquivos = _listar_arquivos(
+        cliente_ftp=cliente_ftp_siasus,
+        arquivo_nome_ou_padrao=arquivo_nome_ou_padrao,
+    )
+    assert hasattr(lista_arquivos, "__iter__")
+    assert all(isinstance(arquivo, str) for arquivo in lista_arquivos)
+    assert len(lista_arquivos) > 0
+
+
+@pytest.mark.parametrize(
+    "arquivo_nome_ou_padrao",
+    ["PAZZ2108.dbc", re.compile("PAZZ1112[a-z]?.dbc", re.IGNORECASE)]
+)
+def teste_listar_arquivos_inexistentes(
+    cliente_ftp_siasus,
+    arquivo_nome_ou_padrao,
+):
+    with pytest.raises(error_perm):
+        lista_arquivos = _listar_arquivos(
+            cliente_ftp=cliente_ftp_siasus,
+            arquivo_nome_ou_padrao=arquivo_nome_ou_padrao,
+        )
 
 
 @pytest.mark.parametrize(
