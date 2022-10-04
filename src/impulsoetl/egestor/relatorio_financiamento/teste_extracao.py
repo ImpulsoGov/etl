1abb0529e35d34eff31d8e1be72fed8f6020dd17
Maira <maaottoni@gmail.com>
2022-10-04 15:07:05 -0300

    Atualização na função de extração

--- src/impulsoetl/egestor/relatorio_financiamento/teste_extracao.py
+++ src/impulsoetl/egestor/relatorio_financiamento/teste_extracao.py
@@ -1,7 +1,7 @@
 """ Extrai relatório de financiamento a partir do egestor"""
-
-from asyncio.windows_events import NULL
+import time
 from time import sleep
+from asyncio.windows_events import NULL
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
@@ -9,51 +9,42 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium_driver_updater import DriverUpdater
 
+import pandas as pd
+
 import sys
-sys.path.append("/Users/maira/Documents/ImpulsoGov/etl/src/impulsoetl")
+sys.path.append(r'C:\Users\maira\Impulso\etl\src')
 from impulsoetl.navegadores import listar_downloads
 
-# Atribui o caminho para a pagina do relatorio de financiamento do egestor
-egestorFinanciamento = 'https://egestorab.saude.gov.br/gestaoaps/relFinanciamentoParcela.xhtml'
+def extracao(periodo):
+    # Cria um um WebDriver 
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
 
-# Cria um objeto para para manipular um WebDriver Chrome com Selenium
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.maximize_window()
-driver.get(egestorFinanciamento)
+    # Atribui o caminho para a pagina do relatorio de financiamento do egestor  
+    egestorFinanciamento = 'https://egestorab.saude.gov.br/gestaoaps/relFinanciamentoParcela.xhtml'
+    driver.get(egestorFinanciamento)
 
-# Seletor para UF
-selectUF = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:uf')
-Select(selectUF).select_by_visible_text('** TODOS **')
-time.sleep(5)  # esperar um pouco pela resposta do servidor
+    # Seletor para UF
+    selectUF = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:uf')
+    Select(selectUF).select_by_visible_text('** TODOS **')  
+    time.sleep(5)  # esperar um pouco pela resposta do servidor
 
-# Seletor para Ano
-selectAno = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:ano')
-Select(selectAno).select_by_visible_text('2022')
-time.sleep(5)
+    # Seletor para Ano
+    selectAno = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:ano')
+    Select(selectAno).select_by_visible_text('2022')
+    time.sleep(5)
 
-#Seletor para Parcela
-selectParcela = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:compInicio')
-Select(selectParcela).select_by_visible_text('SET/2022')
-time.sleep(5)
+    #Seletor para Parcela
+    selectParcela = driver.find_element(By.CSS_SELECTOR,'#j_idt58\:compInicio')
+    Select(selectParcela).select_by_visible_text(periodo)
+    time.sleep(5)
 
-# Baixa o relatório clicando no botão download
-botaoDownload = driver.find_element(By.CLASS_NAME,'btn-app')
-driver.execute_script("arguments[0].click();", botaoDownload)
-time.sleep(40) # espera para concluir o download do relatório
+    # Baixa o relatório clicando no botão download
+    botaoDownload = driver.find_element(By.CLASS_NAME,'btn-app')
+    driver.execute_script("arguments[0].click();", botaoDownload)
+    time.sleep(40) # espera para concluir o download do relatório
 
-# Verificar e garantir que o relatório seja baixado somente uma vez
-downloads_antes = listar_downloads()
-# checar a cada 1 segundo se o relatório foi baixado
-contador = 0
-arquivo_baixado = None
+    return True
 
-while contador < ESPERA_MAX:
-# esperar um pouco pela resposta do servidor
-    sleep(2)
-    contador += 1
-
-# checar se o relatório já foi baixado; interromper laço se sim
-    arquivos_baixados = listar_downloads().difference(downloads_antes)
-    if arquivos_baixados:
-        arquivo_baixado = arquivos_baixados.pop()
-        break
+periodo = 'SET/2022'
+df_extraido = extracao(periodo)
