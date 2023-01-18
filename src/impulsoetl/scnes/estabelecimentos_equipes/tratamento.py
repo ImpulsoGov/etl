bdbfb6bc65c50085877571a0f1552b17f3d7f5c1
maira <maaottoni@gmail.com>
2023-01-18 15:06:35 -0300

    Atualiza tratamento dos dados

--- src/impulsoetl/scnes/estabelecimentos_equipes/tratamento.py
+++ src/impulsoetl/scnes/estabelecimentos_equipes/tratamento.py
@@ -19,14 +19,6 @@ COLUNAS_EXCLUIR = [
     'coArea', 
     'coMunicipio',
     'dsArea', 
-    'quilombola', 
-    'assentada', 
-    'geral', 
-    'escola', 
-    'pronasci',
-    'indigena', 
-    'ribeirinha', 
-    'complem', 
     'seqEquipe',
     'tpEquipe'
 ]
@@ -35,8 +27,17 @@ COLUNAS_RENOMEAR: Final[dict[str, str]] = {
     'dsEquipe':'equipe_tipo',
     'coEquipe':'equipe_id_ine', 
     'nomeEquipe':'equipe_nome', 
+    'quilombola':'equipe_atencao_quilombola', 
+    'assentada':'equipe_atencao_assentada', 
+    'geral':'equipe_atencao_geral', 
+    'escola':'equipe_atencao_escola', 
+    'pronasci':'equipe_atencao_pronasci',
+    'indigena':'equipe_atencao_indigena', 
+    'ribeirinha':'equipe_atencao_ribeirinha', 
+    'complem':'equipe_atencao_complementar', 
     'dtAtivacao':'periodo_ativacao', 
-    'dtDesativacao':'periodo_desativacao'
+    'dtDesativacao':'periodo_desativacao',
+    
 }
 
 COLUNAS_TIPOS: Final[frozendict] = frozendict ({
@@ -46,6 +47,14 @@ COLUNAS_TIPOS: Final[frozendict] = frozendict ({
     'equipe_tipo':'str', 
     'equipe_tipo':'str',
     'equipe_nome':'str', 
+    'equipe_atencao_quilombola':'int', 
+    'equipe_atencao_assentada':'int', 
+    'equipe_atencao_geral':'int', 
+    'equipe_atencao_escola':'int', 
+    'equipe_atencao_pronasci':'int',
+    'equipe_atencao_indigena':'int', 
+    'equipe_atencao_ribeirinha':'int', 
+    'equipe_atencao_complementar':'int', 
     'periodo_ativacao':'str', 
     'periodo_desativacao':'str'
 }
@@ -77,7 +86,7 @@ def tratar_tipos(df_extraido: pd.DataFrame) -> pd.DataFrame:
 def tratar_tipos(df_extraido: pd.DataFrame) -> pd.DataFrame:
     for coluna in COLUNAS_DATA:
         df_extraido[coluna] = pd.to_datetime(
-            df_extraido[coluna], infer_datetime_format=True
+            df_extraido[coluna], infer_datetime_format=True, errors = 'coerce'
         )
 
     df_extraido = df_extraido.astype(COLUNAS_TIPOS, errors="ignore").where(
@@ -99,20 +108,25 @@ def tratamento_dados(
 
     logger.info("Iniciando o tratamento dos dados ...")
 
-    df_extraido = excluir_colunas(df_extraido)
-    df_extraido = renomear_colunas(df_extraido)
-    df_extraido = ordenar_colunas(df_extraido, COLUNAS_TIPOS)
-    df_extraido = tratar_tipos(df_extraido)
-    df_extraido["periodo_id"] = periodo_id
-    df_extraido["unidade_geografica_id"] = unidade_geografica_id
-    df_extraido = df_extraido.reset_index(drop=True)
+    try:
+        df_extraido = excluir_colunas(df_extraido)
+        df_extraido = renomear_colunas(df_extraido)
+        df_extraido = ordenar_colunas(df_extraido, COLUNAS_TIPOS)
+        df_extraido = tratar_tipos(df_extraido)
+        df_extraido["periodo_id"] = periodo_id
+        df_extraido["unidade_geografica_id"] = unidade_geografica_id
+        df_extraido = df_extraido.reset_index(drop=True)
 
-    logger.info("Dados transformados ...")
+        logger.info("Dados transformados ...")
+    
+    except Exception as e:
+        print(e)
+        logger.info("Erro ao transformar os dados")
+
 
     return df_extraido
 
-
-#codigo_municipio = '120025'
+#codigo_municipio =  '110015'
 #periodo_id = '2023'
 #unidade_geografica_id = 'brasil00000'
 #lista_codigos = extrair_lista_cnes(codigo_municipio)
