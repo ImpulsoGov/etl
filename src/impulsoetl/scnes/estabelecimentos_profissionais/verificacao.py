0fc9f3f94ec1847421accfacc977f907f4ec5ca4
maira <maaottoni@gmail.com>
2023-01-19 14:59:49 -0300

    Atualiza função de verificação do ETL dos profissionais do SCNES

--- src/impulsoetl/scnes/estabelecimentos_profissionais/verificacao.py
+++ src/impulsoetl/scnes/estabelecimentos_profissionais/verificacao.py
@@ -2,9 +2,6 @@ import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
-from frozendict import frozendict
-from typing import Final
-from datetime import date
 
 from impulsoetl.scnes.extracao_lista_cnes import extrair_lista_cnes
 from impulsoetl.scnes.estabelecimentos_equipes.extracao import extrair_equipes
@@ -25,34 +22,12 @@ def verifica_diferenca_qtd_registros(
         == 0
     )
 
-def verifica_profissionais_duplicados (
-    df_extraido: pd.DataFrame,
-) ->bool:
-    """Verifica se há numero de cns duplicados"""
-    return (
-        df_tratado['profissional_cns'].is_unique
-    )
-
-
-
-def verificar_informacoes_estabelecimentos_identicados(
+def verificar_dados(
     df_extraido: pd.DataFrame,
     df_tratado: pd.DataFrame,
 ) -> None:
     
     logger.info("Iniciando a verificação dos dados ... ")
     assert verifica_diferenca_qtd_registros(df_extraido, df_tratado)
-    assert verifica_profissionais_duplicados(df_tratado)
     logger.info("Dados verificados corretamente")
 
-
-
-
-
-#codigo_municipio = '120025'
-#periodo_id = '2023'
-#unidade_geografica_id = 'brasil00000'
-#lista_codigos = extrair_lista_cnes(codigo_municipio)
-#df_extraido = extrair_profissionais(codigo_municipio, lista_codigos)
-#df_tratado = tratamento_dados(df_extraido, periodo_id, unidade_geografica_id)
-#df_verificado = verificar_informacoes_estabelecimentos_identicados(df_extraido,df_tratado)
