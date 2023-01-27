4643f849f3fabc59fdb1a0d23cbeeacd862d438b
maira <maaottoni@gmail.com>
2023-01-27 12:20:29 -0300

    Inclui função para transformar os dados de horários em json

--- src/impulsoetl/scnes/estabelecimentos_horarios/tratamento.py
+++ src/impulsoetl/scnes/estabelecimentos_horarios/tratamento.py
@@ -1,14 +1,21 @@
 from typing import Final
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import json
 
 import pandas as pd
 from frozendict import frozendict
 from prefect import task
 
-from impulsoetl.scnes.estabelecimentos_horarios.extracao import extrair_horarios_estabelecimentos
+from impulsoetl.scnes.estabelecimentos_horarios.extracao import (
+    extrair_horarios_estabelecimentos,
+)
 from impulsoetl.scnes.extracao_lista_cnes import extrair_lista_cnes
 from impulsoetl.loggers import logger, habilitar_suporte_loguru
 
-COLUNAS_EXCLUIR = ["diaSemana", "hrInicioAtendimento", "hrFimAtendimento"]
+# COLUNAS_EXCLUIR = ["diaSemana", "hrInicioAtendimento", "hrFimAtendimento","horario"]
 
 COLUNAS_TIPOS: Final[frozendict] = frozendict(
     {
@@ -31,6 +38,38 @@ def tratar_tipos(df_extraido: pd.DataFrame) -> pd.DataFrame:
     )
 
 
+def transformar_horarios_json(df_extraido: pd.DataFrame) -> pd.DataFrame:
+    df_extraido["horario"] = (
+        +df_extraido["hrInicioAtendimento"]
+        + "-"
+        + df_extraido["hrFimAtendimento"]
+    )
+
+    df_consolidado = pd.DataFrame()
+
+    lista_cnes = (
+        df_extraido["estabelecimento_cnes_id"].value_counts().index.tolist()
+    )
+
+    for cnes in lista_cnes:
+        df = df_extraido.loc[df_extraido["estabelecimento_cnes_id"] == cnes]
+        df["horario_funcionamento"] = json.dumps(
+            [{a: b} for a, b in zip(df["diaSemana"], df["horario"])],
+            ensure_ascii=False,
+        )
+        df = df[
+            [
+                "municipio_id_sus",
+                "estabelecimento_cnes_id",
+                "horario_funcionamento",
+            ]
+        ]
+        df = df.drop_duplicates().reset_index(drop=True)
+        df_consolidado = df_consolidado.append(df)
+
+    return df_consolidado
+
+
 def tratamento_dados(
     df_extraido: pd.DataFrame, periodo_id: str, unidade_geografica_id: str
 ) -> pd.DataFrame:
@@ -44,22 +83,13 @@ def tratamento_dados(
      Retorna:
         Objeto [`pandas.DataFrame`] com os dados enriquecidos e tratados.
     """
-    #habilitar_suporte_loguru()
+    # habilitar_suporte_loguru()
     logger.info("Iniciando o tratamento dos dados ...")
 
-    df_extraido["horario_funcionamento"] = (
-        df_extraido["diaSemana"]
-        + " "
-        + df_extraido["hrInicioAtendimento"]
-        + "-"
-        + df_extraido["hrFimAtendimento"]
-    )
-    df_extraido = df_extraido.groupby(
-        ["municipio_id_sus", "estabelecimento_cnes_id"], as_index=False
-    ).agg({"horario_funcionamento": " ".join})
-    df_extraido["periodo_id"] = periodo_id
-    df_extraido["unidade_geografica_id"] = unidade_geografica_id
-    df_tratado = df_extraido.reset_index(drop=True)
+    df = transformar_horarios_json(df_extraido)
+    df["periodo_id"] = periodo_id
+    df["unidade_geografica_id"] = unidade_geografica_id
+    df_tratado = df.reset_index(drop=True)
     tratar_tipos(df_tratado)
 
     logger.info("Dados transformados ...")
