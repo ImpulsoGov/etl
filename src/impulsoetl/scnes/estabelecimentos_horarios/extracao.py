7f647064367ed4765e51455269da2fbc740e8cb6
maira <maaottoni@gmail.com>
2023-02-13 14:46:26 -0300

    Inclui condições para identificar os horários dos estabelecimentos que tem funcionamento indicado como 'Sempre Aberto'

--- src/impulsoetl/scnes/estabelecimentos_horarios/extracao.py
+++ src/impulsoetl/scnes/estabelecimentos_horarios/extracao.py
@@ -46,7 +46,6 @@ def extrair_horarios_estabelecimentos(
                 + cnes
                 + "?competencia={:%Y%m}".format(periodo_data_inicio)
             )
-            # url = "http://cnes.datasus.gov.br/services/estabelecimentos/atendimento/"+codigo_municipio+cnes
             payload = {}
             headers = {
                 "Accept": "application/json, text/plain, */*",
@@ -66,6 +65,35 @@ def extrair_horarios_estabelecimentos(
             df["municipio_id_sus"] = codigo_municipio
             df["estabelecimento_cnes_id"] = cnes
             df_extraido = df_extraido.append(df)
+
+            if  df.empty:
+                try:
+                    url = 'https://cnes.datasus.gov.br/services/estabelecimentos/'+codigo_municipio+cnes+"?competencia={:%Y%m}".format(periodo_data_inicio)
+                    payload = {}
+                    headers = {
+                        "Accept": "application/json, text/plain, */*",
+                        "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+                        "Connection": "keep-alive",
+                        "Referer": "http://cnes.datasus.gov.br/pages/estabelecimentos/",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+                        }
+                    response = requests.request("GET", url, headers=headers, data=payload)
+                    res = response.text
+                    parsed = json.loads(res)
+
+                    if "tpSempreAberto"  in parsed:
+                        horario_sempre_aberto = parsed.get("tpSempreAberto")     
+                    if horario_sempre_aberto == "S":
+                        df_parcial = pd.DataFrame()
+                        df_parcial['municipio_id_sus'] = [codigo_municipio]
+                        df_parcial['estabelecimento_cnes_id'] = [cnes]
+                        df_parcial['diaSemana'] = 'Sempre Aberto'
+                        df_parcial['hrInicioAtendimento'] = ""
+                        df_parcial['hrFimAtendimento'] = ""
+                        df_extraido = df_extraido.append(df_parcial)
+                
+                except json.JSONDecodeError:
+                    pass  
 
         except json.JSONDecodeError:
             logger.info(
