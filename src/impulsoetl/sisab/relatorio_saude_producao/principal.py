6ecb3ac46b06216360d2a5488089a892843f9171
maira <maaottoni@gmail.com>
2023-02-15 16:19:47 -0300

    Corrige módulos de importação da função principal

--- src/impulsoetl/sisab/relatorio_saude_producao/principal.py
+++ src/impulsoetl/sisab/relatorio_saude_producao/principal.py
@@ -9,8 +9,10 @@ from sqlalchemy.orm import Session
 
 from impulsoetl import __VERSION__
 from impulsoetl.bd import Sessao
-from impulsoetl.sisab.relatorio_saude_producao.extracao import extrair_relatorio_saude_producao
+from impulsoetl.sisab.relatorio_saude_producao.extracao import extrair_relatorio
 from impulsoetl.sisab.relatorio_saude_producao.tratamento import tratamento_dados
+from impulsoetl.sisab.relatorio_saude_producao.verificacao import verificar_informacoes_relatorio_producao
+
 from impulsoetl.utilitarios.bd import carregar_dataframe
 
 def obter_relatorio_producao_por_profissional_problema_conduta_atendimento(
@@ -22,17 +24,17 @@ def obter_relatorio_producao_por_profissional_problema_conduta_atendimento(
 ) -> None:
 
 
-    df_extraido = extrair_relatorio_saude_producao(
+    df_extraido = extrair_relatorio(
         periodo_competencia = periodo_competencia
     )
-    
-    print(df_extraido)
-    
+        
     df_tratado = tratamento_dados(
         df_extraido=df_extraido,
         periodo_id=periodo_id,
         unidade_geografica_id=unidade_geografica_id,
     )
+
+    verificar_informacoes_relatorio_producao(df_tratado)
 
     carregar_dataframe(
         sessao=sessao, df=df_tratado, tabela_destino=tabela_destino
