e8ed01f229d16f7f1861361bfbfc1c843c85564f
maira <maaottoni@gmail.com>
2023-04-03 16:56:15 -0300

    Refatora função que extrai os profissionais com INE

--- src/impulsoetl/scnes/estabelecimentos_profissionais/extracao.py
+++ src/impulsoetl/scnes/estabelecimentos_profissionais/extracao.py
@@ -6,7 +6,7 @@ from datetime import date
 
 import pandas as pd
 import requests
-#from prefect import task
+from prefect import task
 
 from impulsoetl.loggers import habilitar_suporte_loguru, logger
 from impulsoetl.scnes.estabelecimentos_equipes.extracao import extrair_equipes
@@ -15,7 +15,6 @@ from impulsoetl.scnes.extracao_lista_cnes import extrair_lista_cnes
 
 def extrair_profissionais_com_ine (
     codigo_municipio:str,
-    lista_codigos:list,
     periodo_data_inicio:date
 )->pd.DataFrame:
     """
@@ -28,47 +27,47 @@ def extrair_profissionais_com_ine (
      Retorna:
         Objeto [`pandas.DataFrame`] com os dados extraídos.
     """
+
+    # Extrai as equipes do municipio
+    lista_cnes = extrair_lista_cnes(codigo_municipio)
+    equipes = extrair_equipes(codigo_municipio,lista_cnes,periodo_data_inicio).reset_index()
+    linhas = equipes.shape[0]
+
     df_extraido = pd.DataFrame()
 
-    lista_codigos = extrair_lista_cnes(codigo_municipio)
-    equipes = extrair_equipes(codigo_municipio, lista_codigos, periodo_data_inicio)
-
-    for cnes in lista_codigos:
-        equipes_cnes = equipes.loc[equipes['estabelecimento_cnes_id']==cnes]
-        codigos_equipe_area = dict(zip(equipes_cnes['seqEquipe'],equipes_cnes['coArea']))
-        codigos_equipe_cnes =  dict(zip(equipes_cnes['seqEquipe'],equipes_cnes['coEquipe']))
-
-        for seqEquipe in codigos_equipe_area:
-            coArea = codigos_equipe_area[seqEquipe]
-            for coEquipe in codigos_equipe_cnes:
-                coEquipe = codigos_equipe_cnes[coEquipe]
-
-                try:
-                    url = "http://cnes.datasus.gov.br/services/estabelecimentos-equipes/profissionais/"+codigo_municipio+cnes+"?coMun="+codigo_municipio+"&coArea="+coArea+"&coEquipe="+seqEquipe+"&competencia={:%Y%m}".format(periodo_data_inicio)
-                    payload={}
-                    headers = {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                    'Connection': 'keep-alive',
-                    'Referer': 'http://cnes.datasus.gov.br/pages/estabelecimentos/ficha/equipes/',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-                    }
-
-                    response = requests.request("GET", url, headers=headers, data=payload)
-                    res = response.text
+    # Para cada equipe seleciona a seqEquipe (número INE sem os zeros no inicio) e o coArea, e extrai os profissionais vinculados àquela equipe:
+    cont = 0
+    while cont <= (linhas-1):
+        cnes = equipes.loc[cont,'estabelecimento_cnes_id']
+        seqEquipe = equipes.loc[cont, 'seqEquipe']
+        coArea = equipes.loc[cont, 'coArea']
+        coEquipe = equipes.loc[cont, 'coEquipe']
         
-                    parsed = json.loads(res)
-                    df = pd.DataFrame(parsed)
-                    df['INE'] = coEquipe
-                    df['coArea'] = coArea
-                    df['estabelecimento_cnes_id'] = cnes
-                    df['municipio_id_sus'] = codigo_municipio
+        try:
+            url = "http://cnes.datasus.gov.br/services/estabelecimentos-equipes/profissionais/"+codigo_municipio+cnes+"?coMun="+codigo_municipio+"&coArea="+coArea+"&coEquipe="+seqEquipe+"&competencia={:%Y%m}".format(periodo_data_inicio)
+            payload={}
+            headers = {
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Connection': 'keep-alive',
+                'Referer': 'http://cnes.datasus.gov.br/pages/estabelecimentos/ficha/equipes/',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+                }
 
-                    df_extraido = df_extraido.append(df)
+            response = requests.request("GET", url, headers=headers, data=payload)
+            res = response.text
+            parsed = json.loads(res)
+            df = pd.DataFrame(parsed)
+            df['INE'] = coEquipe
+            df['coArea'] = coArea
+            df['estabelecimento_cnes_id'] = cnes
+            df_extraido = df_extraido.append(df)
+            cont += 1
 
-                except json.JSONDecodeError:
-                    logger.error("Erro ao extrair os profissionais com INE")
-                    pass
+        except Exception as e:
+            logger.error(e)
+            logger.error("Erro ao extrair os profissionais com INE")
+            pass
             
     return df_extraido    
 
@@ -128,11 +127,10 @@ def extrair_profissionais (
             logger.error("Erro ao extrair os profissionais")
             pass
     
-    df_ine = extrair_profissionais_com_ine(codigo_municipio=codigo_municipio,lista_codigos=lista_codigos, periodo_data_inicio=periodo_data_inicio)
-    df_ine = df_ine[['estabelecimento_cnes_id','INE','dtEntrada','dtDesligamento','cns','municipio_id_sus']]
-    df_consolidado = pd.merge(df_parcial, df_ine, how='outer', on=['cns','estabelecimento_cnes_id','municipio_id_sus'])
+    df_ine = extrair_profissionais_com_ine(codigo_municipio=codigo_municipio, periodo_data_inicio=periodo_data_inicio)
+    df_ine = df_ine[['estabelecimento_cnes_id','INE','dtEntrada','dtDesligamento','cns']]
+    df_consolidado = pd.merge(df_parcial, df_ine, how='outer', on=['cns','estabelecimento_cnes_id'])
 
     logger.info("Extração concluída ...")
 
     return df_consolidado
-
