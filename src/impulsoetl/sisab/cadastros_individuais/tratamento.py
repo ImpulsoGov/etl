1577ae7bdb724d25c030895ae2bc76c307c91905
WaltMath <wmatheus.97@gmail.com>
2023-05-09 21:54:21 -0300

    Exclui etapas de tratamento e inclui novas funções

--- src/impulsoetl/sisab/cadastros_individuais/tratamento.py
+++ src/impulsoetl/sisab/cadastros_individuais/tratamento.py
@@ -2,18 +2,93 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import Final
 
-from datetime import date, datetime
-
+from frozendict import frozendict
 import pandas as pd
 from prefect import task
 from sqlalchemy.orm import Session
-from uuid6 import uuid7
 
-from impulsoetl.comum.datas import periodo_por_codigo, periodo_por_data
 from impulsoetl.comum.geografias import id_sus_para_id_impulso
-from impulsoetl.loggers import habilitar_suporte_loguru
+from impulsoetl.loggers import logger,habilitar_suporte_loguru
 
+CADASTROS_COLUNAS_TIPOS: Final[frozendict] = frozendict(
+    {
+        "municipio_id_sus": str,
+        "periodo_id": str,
+        "cnes_id": str,
+        "cnes_nome": str,
+        "equipe_id_ine": str,
+        "quantidade": "int64",
+        "periodo_codigo": str,
+        "criterio_pontuacao": bool,
+        "unidade_geografica_id": str,
+    }
+)
+
+CADASTROS_COLUNAS: Final[dict[str, str]] = {
+    "IBGE": "municipio_id_sus",
+    "CNES": "cnes_id",
+    "Estabelecimento": "cnes_nome",
+    "INE": "equipe_id_ine",
+}
+
+def renomear_colunas(
+    df_extraido:pd.DataFrame,
+    ):
+    return df_extraido.rename(columns=CADASTROS_COLUNAS).rename(columns={df_extraido.columns[7]: 'quantidade'})
+
+def excluir_colunas(
+    df_tratado:pd.DataFrame,
+    ):
+    
+    return (df_tratado.drop(
+        ["Uf", "Municipio","Sigla da equipe", "Unnamed: 8"],
+        axis=1
+    ).dropna())
+
+
+def garantir_tipos_colunas(
+    df_tratado:pd.DataFrame,
+    ):
+
+    return df_tratado.astype(CADASTROS_COLUNAS_TIPOS)
+
+def definir_coluna_criterio_pontuacao(
+    df_tratado:pd.DataFrame,
+    com_ponderacao:bool,
+    ):
+
+    return df_tratado.insert(1, "criterio_pontuacao", com_ponderacao, allow_duplicates = True)
+
+def definir_coluna_periodo_codigo(
+    df_tratado:pd.DataFrame,
+    periodo_codigo:str,
+    ):
+
+    return df_tratado.insert(1, "periodo_codigo", periodo_codigo, allow_duplicates = True)
+
+def definir_coluna_periodo_id(
+    df_tratado:pd.DataFrame,
+    periodo_id:str,
+    ):
+
+    return df_tratado.insert(1, "periodo_id", periodo_id, allow_duplicates = True)
+
+def definir_coluna_unidade_geografica_id(
+    df_tratado:pd.DataFrame,
+    sessao:Session,
+    ):
+    
+    df_tratado["unidade_geografica_id"] = df_tratado[
+        "municipio_id_sus"
+    ].apply(
+        lambda municipio_id_sus: id_sus_para_id_impulso(
+            sessao=sessao,
+            id_sus=municipio_id_sus,
+        )
+    )
+    return df_tratado
 
 @task(
     name="Transformar Cadastros Individuais",
@@ -26,98 +101,49 @@ from impulsoetl.loggers import habilitar_suporte_loguru
     retries=0,
     retry_delay_seconds=None,
 )
-def tratamento_dados(
+
+def tratar_dados(
     sessao: Session,
-    dados_sisab_cadastros: pd.DataFrame,
+    df_extraido: pd.DataFrame,
     com_ponderacao: bool,
-    periodo: date,
+    periodo_id:str,
+    periodo_codigo:str,
 ) -> pd.DataFrame:
+    """Inclui todas etapas de transformação dos dados de cadastros de equipes pelo SISAB.
+
+    Argumentos:
+        sessao: objeto [`sqlalchemy.orm.session.Session`][] que permite
+            acessar a base de dados da ImpulsoGov.
+        df_extraido: DataFrame do relatŕorio extraído no SISAB.
+        visao_equipe: Indica a situação da equipe considerada para a contagem
+            dos cadastros.
+        com_ponderacao: Lista de booleanos indicando quais tipos de população
+            devem ser filtradas no cadastro - onde `True` indica apenas as
+            populações com critério de ponderação e `False` indica todos os
+            cadastros. Por padrão, o valor é `[True, False]`, indicando que
+            ambas as possibilidades são extraídas do SISAB e carregadas para a
+            mesma tabela de destino.
+        periodo_id: Identificador único do período referente ao mês/ano de disponibilização do relatório.
+        periodo_codigo: Código do período referente ao mês/ano de disponibilização do relatório.
+    """
+
     habilitar_suporte_loguru()
-    tabela_consolidada = pd.DataFrame(
-        columns=[
-            "id",
-            "municipio_id_sus",
-            "periodo_id",
-            "periodo_codigo",
-            "cnes_id",
-            "cnes_nome",
-            "equipe_id_ine",
-            "quantidade",
-            "criterio_pontuacao",
-            "criacao_data",
-            "atualizacao_data",
-        ]
-    )
+    
+    logger.info("Renomeando colunas da tabela...")
+    print(df_extraido)
+    df_tratado = renomear_colunas(df_extraido=df_extraido)
 
-    periodo_cod = periodo_por_data(sessao=sessao, data=periodo)
-    tabela_consolidada[
-        [
-            "municipio_id_sus",
-            "cnes_id",
-            "cnes_nome",
-            "equipe_id_ine",
-            "quantidade",
-        ]
-    ] = dados_sisab_cadastros.loc[
-        :, ["IBGE", "CNES", "Nome UBS", "INE", "quantidade"]
-    ]
-    tabela_consolidada["criterio_pontuacao"] = com_ponderacao
-    tabela_consolidada["periodo_codigo"] = periodo_cod[3]
-    tabela_consolidada.reset_index(drop=True, inplace=True)
-    tabela_consolidada["id"] = tabela_consolidada.apply(
-        lambda row: uuid7(), axis=1
-    )
-    tabela_consolidada["criacao_data"] = datetime.now().strftime(
-        "%Y-%m-%d %H:%M:%S"
-    )
-    tabela_consolidada["atualizacao_data"] = datetime.now().strftime(
-        "%Y-%m-%d %H:%M:%S"
-    )
+    logger.info("Exluindo colunas não úteis da tabela...")
+    df_tratado = excluir_colunas(df_tratado=df_tratado)
 
-    periodo_obj = periodo_por_codigo(sessao=sessao, codigo=periodo_cod[3])
-    tabela_consolidada["periodo_id"] = periodo_obj.id
-    tabela_consolidada["unidade_geografica_id"] = tabela_consolidada[
-        "municipio_id_sus"
-    ].apply(
-        lambda municipio_id_sus: id_sus_para_id_impulso(
-            sessao=sessao,
-            id_sus=municipio_id_sus,
-        )
-    )
+    logger.info("Ennquicimento de tabela com novas colunas...")
+    definir_coluna_criterio_pontuacao(df_tratado=df_tratado,com_ponderacao=com_ponderacao)
+    definir_coluna_periodo_codigo(df_tratado=df_tratado,periodo_codigo=periodo_codigo)
+    definir_coluna_periodo_id(df_tratado=df_tratado,periodo_id=periodo_id)
+    definir_coluna_unidade_geografica_id(df_tratado=df_tratado,sessao=sessao)
 
-    tabela_consolidada["id"] = tabela_consolidada["id"].astype("string")
-    tabela_consolidada["municipio_id_sus"] = tabela_consolidada[
-        "municipio_id_sus"
-    ].astype("string")
-    tabela_consolidada["periodo_id"] = tabela_consolidada["periodo_id"].astype(
-        "string"
-    )
-    tabela_consolidada["periodo_codigo"] = tabela_consolidada[
-        "periodo_codigo"
-    ].astype("string")
-    tabela_consolidada["cnes_id"] = tabela_consolidada["cnes_id"].astype(
-        "string"
-    )
-    tabela_consolidada["cnes_nome"] = tabela_consolidada["cnes_nome"].astype(
-        "string"
-    )
-    tabela_consolidada["unidade_geografica_id"] = tabela_consolidada[
-        "unidade_geografica_id"
-    ].astype("string")
-    tabela_consolidada["equipe_id_ine"] = tabela_consolidada[
-        "equipe_id_ine"
-    ].astype("string")
-    tabela_consolidada["quantidade"] = tabela_consolidada["quantidade"].astype(
-        int
-    )
-    tabela_consolidada["criterio_pontuacao"] = tabela_consolidada[
-        "criterio_pontuacao"
-    ].astype(bool)
-    tabela_consolidada["criacao_data"] = tabela_consolidada[
-        "criacao_data"
-    ].astype("string")
-    tabela_consolidada["atualizacao_data"] = tabela_consolidada[
-        "atualizacao_data"
-    ].astype("string")
+    logger.info("Garantindo tipagem dos dados...")
+    garantir_tipos_colunas(df_tratado=df_tratado)
 
-    return tabela_consolidada
+    df_tratado.reset_index(drop=True, inplace=True)
+    return df_tratado
