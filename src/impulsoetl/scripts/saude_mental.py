2aab56c3afe7d2cca9755d787214df352b7b4f04
bcbernardo <bernardo@impulsogov.org>
2023-07-25 18:40:35 -0300

    feat: :card_file_box: Adiciona exemplo de uso de semáforo em script de ETL

--- src/impulsoetl/scripts/saude_mental.py
+++ src/impulsoetl/scripts/saude_mental.py
@@ -20,6 +20,12 @@ from impulsoetl.sihsus.aih_rd import obter_aih_rd
 from impulsoetl.sinan.violencia import obter_agravos_violencia
 from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.principal import obter_relatorio_resolutividade_por_condicao
 from impulsoetl.sisab.relatorio_tipo_equipe_por_tipo_producao.principal import obter_relatorio_tipo_equipe_por_producao
+from impulsoetl.utilitarios.semaforos import (
+    bloquear_escrita,
+    checar_escrita_liberada,
+    EscritaBloqueadaExcecao,
+    liberar_escrita,
+)
 
 agendamentos = tabelas["configuracoes.capturas_agendamentos"]
 capturas_historico = tabelas["configuracoes.capturas_historico"]
@@ -58,6 +64,22 @@ def resolutividade_aps_por_condicao(
         )
 
         for agendamento in agendamentos_resolutividade_por_condicao:
+            try:
+                checar_escrita_liberada(
+                    sessao=sessao,
+                    tabela_destino=agendamento.tabela_destino,
+                    unidade_geografica_id=agendamento.unidade_geografica_id,
+                    periodo_id=agendamento.periodo_id,
+                )
+            except EscritaBloqueadaExcecao:
+                logger.warning("Pulando...")
+                continue
+            bloquear_escrita(
+                sessao=sessao,
+                tabela_destino=agendamento.tabela_destino,
+                unidade_geografica_id=agendamento.unidade_geografica_id,
+                periodo_id=agendamento.periodo_id,
+            )
             obter_relatorio_resolutividade_por_condicao(
                 sessao = sessao,
                 tabela_destino = agendamento.tabela_destino,
@@ -76,11 +98,30 @@ def resolutividade_aps_por_condicao(
             )
             conector = sessao.connection()
             conector.execute(requisicao_inserir_historico)
+
             if teste:
                 sessao.rollback()
                 break
-            sessao.commit()
-            logger.info("OK.")
+
+            try:
+                checar_escrita_liberada(
+                    sessao=sessao,
+                    tabela_destino=agendamento.tabela_destino,
+                    unidade_geografica_id=agendamento.unidade_geografica_id,
+                    periodo_id=agendamento.periodo_id,
+                )
+            except EscritaBloqueadaExcecao:
+                logger.error("Revertendo alterações realizadas...")
+                sessao.rollback()
+            else:
+                sessao.commit()
+                liberar_escrita(
+                    sessao=sessao,
+                    tabela_destino=agendamento.tabela_destino,
+                    unidade_geografica_id=agendamento.unidade_geografica_id,
+                    periodo_id=agendamento.periodo_id,
+                )
+                logger.info("OK.")
 
 
 @flow(
