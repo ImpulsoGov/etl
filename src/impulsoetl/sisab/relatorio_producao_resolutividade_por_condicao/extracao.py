8e72fbb34ce14f0fc5ebd0fb816ce3a23e79d8e7
maira <maaottoni@gmail.com>
2023-07-27 16:59:43 -0300

    Refafora função principal do ETL relatorio_producao_resolutividade_por_condicao para ajustar logs quando a competência não estiver disponível

--- src/impulsoetl/sisab/relatorio_producao_resolutividade_por_condicao/extracao.py
+++ src/impulsoetl/sisab/relatorio_producao_resolutividade_por_condicao/extracao.py
@@ -8,19 +8,20 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-import pandas as pd
-
 from datetime import date
+
+import pandas as pd
 from prefect import task
 
-from impulsoetl.sisab.utilitarios_sisab_relatorio_producao import extrair_producao_por_municipio
-from impulsoetl.sisab.utilitarios_sisab_relatorio_producao import transformar_producao_por_municipio
-
-
 from impulsoetl.loggers import logger
-"""
+from impulsoetl.sisab.utilitarios_sisab_relatorio_producao import (
+    extrair_producao_por_municipio,
+    transformar_producao_por_municipio,
+)
+
+
 @task(
-    name= "Extrair Dados de Produção da APS por Tipo de Condição Avaliada e Desfecho",
+    name="Extrair Dados de Produção da APS por Tipo de Condição Avaliada e Desfecho",
     description=(
         "Extrai os dados do relatório de produção da Atenção Primária à Saúde,"
         + "por problema/condição avaliada e desfecho a partir do portal público"
@@ -29,29 +30,28 @@ from impulsoetl.loggers import logger
     tags=["aps", "sisab", "producao", "extracao"],
     retries=2,
     retry_delay_seconds=120,
-)"""
-def extrair_relatorio(
-    periodo_competencia: date)-> pd.DataFrame():
+)
+def extrair_relatorio(periodo_competencia: date) -> pd.DataFrame():
     """
     Extrai relatório de produção por problema/condição avaliada e conduta a partir da página do SISAB
 
      Argumentos:
-        periodo_data_inicio: Data da competência 
+        periodo_data_inicio: Data da competência
      Retorna:
         Objeto [`pandas.DataFrame`] com os dados extraídos.
     """
     df_consolidado = pd.DataFrame()
-    
+
     try:
         df_parcial = extrair_producao_por_municipio(
             tipo_producao="Atendimento Individual",
             competencias=[periodo_competencia],
             selecoes_adicionais={
-                "Problema/Condição Avaliada": "Selecionar Todos", 
-                "Conduta":"Selecionar Todos",
+                "Problema/Condição Avaliada": "Selecionar Todos",
+                "Conduta": "Selecionar Todos",
             },
-            ).pipe(transformar_producao_por_municipio)
-        
+        ).pipe(transformar_producao_por_municipio)
+
         df_consolidado = df_consolidado.append(df_parcial)
 
     except Exception as e:
@@ -59,4 +59,3 @@ def extrair_relatorio(
         pass
 
     return df_consolidado
-
--- src/impulsoetl/sisab/relatorio_producao_resolutividade_por_condicao/principal.py
+++ src/impulsoetl/sisab/relatorio_producao_resolutividade_por_condicao/principal.py
@@ -7,23 +7,33 @@
 import warnings
 
 warnings.filterwarnings("ignore")
+import time
+import traceback
 from datetime import date
 
+import pandas as pd
 from prefect import flow
 from sqlalchemy.orm import Session
 
 from impulsoetl import __VERSION__
-
-from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.extracao import extrair_relatorio
-from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.tratamento import tratamento_dados
-from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.carregamento import carregar_dados
+from impulsoetl.loggers import habilitar_suporte_loguru, logger
+from impulsoetl.sisab.excecoes import SisabExcecao
+from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.carregamento import (
+    carregar_dados,
+)
+from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.extracao import (
+    extrair_relatorio,
+)
+from impulsoetl.sisab.relatorio_producao_resolutividade_por_condicao.tratamento import (
+    tratamento_dados,
+)
 
 
 @flow(
     name="Obter Relatório de Resolutividade da APS por Condição Avaliada",
     description=(
         "Extrai, transforma e carrega os dados de produção da Atenção Primária à Saúde "
-        +"por problema/condição avaliada, a partir do Sistema de Informação em Saúde da Atenção "
+        + "por problema/condição avaliada, a partir do Sistema de Informação em Saúde da Atenção "
         + "Básica do SUS."
     ),
     retries=0,
@@ -42,23 +52,65 @@ def obter_relatorio_resolutividade_por_condicao(
     teste: bool,
 ) -> None:
 
-    df_extraido = extrair_relatorio(
+    tempo_inicio_etl = time.time()
+
+    operacao_id = "0644acff-4642-75e1-b559-6193f928cb16"
+
+    try:
+
+        df_extraido = extrair_relatorio(
+            periodo_competencia=periodo_competencia,
+        )
+
+        df_tratado = tratamento_dados(
+            df_extraido=df_extraido,
+            periodo_id=periodo_id,
+            municipio_id_sus=unidade_geografica_id_sus,
+            unidade_geografica_id=unidade_geografica_id,
+        )
+
+        carregar_dados(
+            sessao=sessao,
+            df_tratado=df_tratado,
+            tabela_destino=tabela_destino,
+            periodo_id=periodo_id,
+            unidade_geografica_id=unidade_geografica_id,
+        )
+
+    except (KeyError, pd.errors.ParserError):
+        traceback_str = traceback.format_exc()
+        enviar_erro = SisabExcecao("Competência indisponível no SISAB")
+        enviar_erro.insere_erro_database(
+            sessao=sessao,
+            traceback_str=traceback_str,
+            operacao_id=operacao_id,
+            periodo_id=periodo_id,
+        )
+
+        logger.error("Data da competência do relatório não está disponível")
+        return 0
+
+    except Exception as mensagem_erro:
+        traceback_str = traceback.format_exc()
+        enviar_erro = SisabExcecao(mensagem_erro)
+        enviar_erro.insere_erro_database(
+            sessao=sessao,
+            traceback_str=traceback_str,
+            operacao_id=operacao_id,
+            periodo_id=periodo_id,
+        )
+
+        logger.error(mensagem_erro)
+        return 0
+
+    tempo_final_etl = time.time() - tempo_inicio_etl
+
+    logger.info(
+        "Terminou ETL para "
+        + "da comepetência`{periodo_competencia}` "
+        + "em {tempo_final_etl}.",
         periodo_competencia=periodo_competencia,
-    )
-
-    df_tratado = tratamento_dados(
-        df_extraido=df_extraido,
-        periodo_id=periodo_id,
-        municipio_id_sus = unidade_geografica_id_sus,
-        unidade_geografica_id=unidade_geografica_id
-    )
-
-    carregar_dados(
-        sessao=sessao, 
-        df_tratado=df_tratado, 
-        tabela_destino=tabela_destino, 
-        periodo_id = periodo_id, 
-        unidade_geografica_id = unidade_geografica_id
+        tempo_final_etl=tempo_final_etl,
     )
 
     return df_tratado
