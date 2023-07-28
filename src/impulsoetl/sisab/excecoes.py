18beedc32a89fa7bb5b206628969342937fe67cd
maira <maaottoni@gmail.com>
2023-07-28 11:24:28 -0300

    Corrige classe SisabErroCompetenciaInexistente

--- src/impulsoetl/sisab/excecoes.py
+++ src/impulsoetl/sisab/excecoes.py
@@ -30,7 +30,7 @@ class SisabErroCompetenciaInexistente:
     def __init__(self,exception):
         self.exception = exception 
 
-    def insere_erro_database(self,sessao,traceback_str,operacao_id,periodo_id):
+    def insere_erro_database(self,sessao,traceback_str,operacao_id,periodo_id,unidade_geografica_id):
 
         if unidade_geografica_id is None:
             unidade_geografica_id = '28de805e-5bdc-49c3-863c-2cf87f95e371'
@@ -38,7 +38,7 @@ class SisabErroCompetenciaInexistente:
             unidade_geografica_id = unidade_geografica_id
 
         tabela_destino = tabelas["configuracoes.capturas_erros_etl"]
-        requisicao_inserir_historico = insert(tabela_destino).values(operacao_id=operacao_id,periodo_id=periodo_id,unidade_geografica_id='28de805e-5bdc-49c3-863c-2cf87f95e371',erro_mensagem=self.exception,erro_traceback=str(traceback_str))
+        requisicao_inserir_historico = insert(tabela_destino).values(operacao_id=operacao_id,periodo_id=periodo_id,unidade_geografica_id=unidade_geografica_id,erro_mensagem=self.exception,erro_traceback=str(traceback_str))
         conector = sessao.connection()
         conector.execute(requisicao_inserir_historico)
         sessao.commit()
