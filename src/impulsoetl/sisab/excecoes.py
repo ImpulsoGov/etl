7f0314058e06590517595aa28ba32b8537817724
maira <maaottoni@gmail.com>
2023-07-28 10:43:01 -0300

    Corrige classe SisabErroCompetenciaInexistente do módulo excecoes.py Corrige classe SisabErroCompetenciaInexistente do módulo excecoes.py para tornar parametrizável a variável unidade_geografica_id

--- src/impulsoetl/sisab/excecoes.py
+++ src/impulsoetl/sisab/excecoes.py
@@ -32,6 +32,11 @@ class SisabErroCompetenciaInexistente:
 
     def insere_erro_database(self,sessao,traceback_str,operacao_id,periodo_id):
 
+        if unidade_geografica_id is None:
+            unidade_geografica_id = '28de805e-5bdc-49c3-863c-2cf87f95e371'
+        else:
+            unidade_geografica_id = unidade_geografica_id
+
         tabela_destino = tabelas["configuracoes.capturas_erros_etl"]
         requisicao_inserir_historico = insert(tabela_destino).values(operacao_id=operacao_id,periodo_id=periodo_id,unidade_geografica_id='28de805e-5bdc-49c3-863c-2cf87f95e371',erro_mensagem=self.exception,erro_traceback=str(traceback_str))
         conector = sessao.connection()
